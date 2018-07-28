01cb8b0808efdda23b5b2e409f2ef6f31407813b
Xin Meng <x.meng@outlook.com>
2018-07-28 18:18:39 +0100

    Initial commit

---
+++ src/main/java/science/mengxin/spring/web/tutorial/SpringWebTutorialApplication.java
@@ -0,0 +1,12 @@
+package science.mengxin.spring.web.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebTutorialApplication.class, args);
+    }
+}
